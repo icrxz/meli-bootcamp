898e93f497325d731ab304e203170074ee732ddd
icrxz <igor.cruz@mercadolivre.com>
2021-07-06 13:05:45 -0300

    add method to add produto

--- module6/aula8/exercise5/src/main/java/com/meli/api/exercise5/controller/PedidoController.java
+++ module6/aula8/exercise5/src/main/java/com/meli/api/exercise5/controller/PedidoController.java
@@ -2,9 +2,12 @@ package com.meli.api.exercise5.controller;
 
 import com.meli.api.exercise5.dao.ClienteDAO;
 import com.meli.api.exercise5.dao.PedidoDAO;
+import com.meli.api.exercise5.dao.ProdutoDAO;
 import com.meli.api.exercise5.dto.PedidoDTO;
+import com.meli.api.exercise5.dto.ProdutoDTO;
 import com.meli.api.exercise5.entity.Cliente;
 import com.meli.api.exercise5.entity.Pedido;
+import com.meli.api.exercise5.entity.Produto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -17,6 +20,7 @@ import java.util.List;
 public class PedidoController {
     ClienteDAO clienteDAO = new ClienteDAO();
     PedidoDAO pedidoDAO = new PedidoDAO();
+    ProdutoDAO produtoDAO = new ProdutoDAO();
 
     @PostMapping
     public ResponseEntity<Pedido> createPedido(@RequestBody PedidoDTO pedidoDTO, UriComponentsBuilder uriBuilder) {
@@ -43,5 +47,16 @@ public class PedidoController {
         List<Pedido> pedidos = pedidoDAO.getList();
 
         return PedidoDTO.converter(pedidos);
+    }
+
+    @PostMapping("/{id}/addProduto/{produtoId}")
+    public PedidoDTO addProduto(@PathVariable long id, @PathVariable long produtoId) {
+        Pedido pedido = pedidoDAO.getPedido(id);
+
+        Produto produto = produtoDAO.getProduto(produtoId);
+
+        pedido.addProduto(produto);
+
+        return PedidoDTO.converter(pedido);
     }
 }
