e335b3c889f8768e9ba852fab484f50d1a3ce754
icrxz <igor.cruz@mercadolivre.com>
2021-07-14 16:43:51 -0300

    feat: create Student repository

---
+++ module10/aula1/diploma-tests/src/main/java/com/meli/api/diplomatests/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.meli.api.diplomatests.repository;
+
+import com.meli.api.diplomatests.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
