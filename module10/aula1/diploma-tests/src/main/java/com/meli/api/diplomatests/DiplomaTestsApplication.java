1cecbee33b7046398319bdac2e7d26a504a9bd4e
icrxz <igor.cruz@mercadolivre.com>
2021-07-14 16:41:44 -0300

    Module 10 - Diploma tests exercise

---
+++ module10/aula1/diploma-tests/src/main/java/com/meli/api/diplomatests/DiplomaTestsApplication.java
@@ -0,0 +1,13 @@
+package com.meli.api.diplomatests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaTestsApplication.class, args);
+    }
+
+}
---
+++ module10/aula1/diploma-tests/src/test/java/com/meli/api/diplomatests/DiplomaTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.api.diplomatests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
